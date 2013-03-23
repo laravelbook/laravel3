16ddab9b0e8938673ff605e0ec1c832b93c8b908
Max Ehsan <contact@laravelbook.com>
2013-03-23 14:31:37 +0600

    Split Symfony components into symfony_lite.php

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@
 // --------------------------------------------------------------
 define('LARAVEL_START', microtime(true));
 # Un-comment out the following line to use the compact version of Laravel
-#define('LARAVEL_LITE', true);
+define('LARAVEL_LITE', true);
 #define('LARAVEL_EXTRA', true);
 #define('SYMFONY_EXTRA', true);
 
